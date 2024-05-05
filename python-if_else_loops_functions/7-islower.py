fc53c8e60abb21a45ea5e2593ddc023c1f536ea0
Tanner Saint <tsaint2004@gmail.com>
2024-05-05 23:49:16 +0000

    changed else to elif

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,6 +3,6 @@ def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         print("{letter} is lower".format(c))
         return True
-    else ord(c) >= 65 and ord(c) <= 90:
+    elif ord(c) >= 65 and ord(c) <= 90:
         print("{letter} is upper".format(c))
         return False
