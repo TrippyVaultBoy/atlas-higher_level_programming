3ca34a5650aee08b6859633dbeffdb029fe2d365
Tanner Saint <tsaint2004@gmail.com>
2024-05-05 23:20:31 +0000

    removed i from print statments

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
     if i == 9:
-        print("{i:02}".format(i))
+        print("{:02}".format(i))
     else:
-        print("{i:02}, ".format(i), end="")
+        print("{:02}, ".format(i), end="")
