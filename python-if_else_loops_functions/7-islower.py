104af772b24fbbbacf7ab67f4a9026d1cc6f46d7
Tanner Saint <tsaint2004@gmail.com>
2024-05-05 23:50:20 +0000

    fixed print statement

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-        print("{letter} is lower".format(c))
+        print("{} is lower".format(c))
         return True
     elif ord(c) >= 65 and ord(c) <= 90:
-        print("{letter} is upper".format(c))
+        print("{} is upper".format(c))
         return False
