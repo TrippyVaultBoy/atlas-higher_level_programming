d36c9a6f6961f7319cf3e2cadab3d3b40bbe90cb
Tanner Saint <tsaint2004@gmail.com>
2024-05-05 03:10:42 +0000

    added else statement

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 for num in range(97, 123):
-    if num != 101 or num != 113:
+    if num == 101 or num == 113:
+    else:
         print("{char}".format(char=chr(num)), end="")
