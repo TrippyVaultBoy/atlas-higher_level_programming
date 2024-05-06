10eeb1869cd181fc939734301759fde9b8787cad
Tanner Saint <tsaint2004@gmail.com>
2024-05-06 00:44:18 +0000

    added format to print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char - 32)))
+            print("{}".format(chr(ord(char - 32))))
         else:
-            print(char)
+            print("".format(char))
