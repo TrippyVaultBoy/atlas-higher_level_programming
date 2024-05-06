0354b547af2a1e1834eedb5f7e597c9b18b3b628
Tanner Saint <tsaint2004@gmail.com>
2024-05-06 02:54:56 +0000

    removed end from print statements

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,11 @@ from sys import argv
 
 def print_args(argv):
     if len(argv) == 1:
-        print("1 argument:", end="")
+        print("1 argument:")
     elif len(argv) > 1:
-        print("{} arguments:".format(len(argv) - 1), end="")
+        print("{} arguments:".format(len(argv) - 1))
     elif len(argv) == 0:
-        print("0 arguments.", end="")
+        print("0 arguments.")
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
