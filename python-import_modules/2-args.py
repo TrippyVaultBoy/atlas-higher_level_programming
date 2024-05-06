4172d769f44dfd264df6e57cadb7211a6b4697b5
Tanner Saint <tsaint2004@gmail.com>
2024-05-06 03:00:58 +0000

    added else statement

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,10 +4,12 @@ from sys import argv
 def print_args(argv):
     if len(argv) == 1:
         print("1 argument:")
-    elif len(argv) > 1:
+    elif len(argv) == 2:
         print("{} arguments:".format(len(argv) - 1))
     elif len(argv) == 0:
         print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
