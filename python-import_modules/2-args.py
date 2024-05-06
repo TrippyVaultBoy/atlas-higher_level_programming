a04bdf10c9937cc45b047ec75e6705d161edf7a8
Tanner Saint <tsaint2004@gmail.com>
2024-05-06 03:09:37 +0000

    removed sys.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 from sys import argv
 
 def print_args(argv):
-    if len(sys.argv) - 1 == 0:
+    if len(argv) - 1 == 0:
         print("0 arguments.")
-    elif len(sys.argv) - 1 == 1:
+    elif len(argv) - 1 == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format((sys.argv) - 1))
+        print("{} arguments:".format((argv) - 1))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
