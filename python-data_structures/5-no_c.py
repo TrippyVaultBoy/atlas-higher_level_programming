c77dcd6b147125d2eba5681bf775575c28b577aa
Tanner Saint <tsaint2004@gmail.com>
2024-05-07 05:08:00 +0000

    fixed new_str declaration

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     new_str = ""
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
-            str += my_string[i]
-    return str
+            new_str += my_string[i]
+    return new_str
