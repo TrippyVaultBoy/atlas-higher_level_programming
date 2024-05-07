6638f8cf63ea728212f77756a339c7b5a425bd23
Tanner Saint <tsaint2004@gmail.com>
2024-05-07 05:07:06 +0000

    added range to for loop

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 def no_c(my_string):
     new_str = ""
-    for i in len(my_string):
+    for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             str += my_string[i]
     return str
