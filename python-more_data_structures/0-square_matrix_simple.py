75a65986378cac01e46cf198e095ee190286d6c5
Tanner Saint <tsaint2004@gmail.com>
2024-05-07 23:44:01 +0000

    changed row name

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,8 @@
 def square_matrix_simple(matrix=[]):
     squares = []
     for row in matrix:
-        row = []
+        squares_row = []
         for element in row:
-            row.append(element ** 2)
-        squares.append(row)
+            squares_row.append(element ** 2)
+        squares.append(squares_row)
     return squares
