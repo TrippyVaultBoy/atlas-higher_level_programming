ae1ca79367cf2c56d797f13d090b6edb26ed5cb1
Tanner Saint <tsaint2004@gmail.com>
2024-05-07 05:21:16 +0000

    added case if matrix is empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            if element != row[-1]:
-                print("{:d}".format(element), end=" ")
-            else:
-                print("{:d}".format(element))
+    if matrix == [[]]:
+        print()
+    else:
+        for row in matrix:
+            for element in row:
+                if element != row[-1]:
+                    print("{:d}".format(element), end=" ")
+                else:
+                    print("{:d}".format(element))
