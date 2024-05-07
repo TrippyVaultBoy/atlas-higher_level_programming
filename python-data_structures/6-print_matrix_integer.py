a8a0a68c6d85fa83a53229d0dca586a123ab7e49
Tanner Saint <tsaint2004@gmail.com>
2024-05-07 05:19:52 +0000

    chnaged single to double quotes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
             if element != row[-1]:
-                print("{:d}".format(element), end=' ')
+                print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element))
