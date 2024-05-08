aa08d355985d96f4c35a201ce2f131823d919fa7
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 01:37:40 +0000

    reverted change

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5
-    , 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     integer = 0
     prev_value = 0
 
