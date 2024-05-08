88b0dcfb885ad3f1832198b88992d585e4f13d14
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 01:15:45 +0000

    added case if empty dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    best_key = None
-    best_score = 0
-    for key in a_dictionary:
-        if a_dictionary[key] > best_score:
-            best_score = a_dictionary[key]
-            best_key = key
-    return best_key
+    if a_dictionary == None:
+        return None
+    else:
+        best_key = None
+        best_score = 0
+        for key in a_dictionary:
+            if a_dictionary[key] > best_score:
+                best_score = a_dictionary[key]
+                best_key = key
+        return best_key
 
