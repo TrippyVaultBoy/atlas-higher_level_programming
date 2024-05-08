89bf0d230ec7e76e5ecd85d2ee91a2c30dfaad2d
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 03:10:30 +0000

    added error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -12,7 +12,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print_count += 1
             except (TypeError, ValueError):
                 pass
-    except TypeError:
+    except (TypeError, ValueError):
         pass
     print()
     return print_count
