a32b1f0b832958aeacf5690c03fa2f2fb1ddaf00
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 03:23:38 +0000

    changed new_list initialization

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = [list_length]
+    new_list = [0] * list_length
 
     for i in range(list_length):
         result = 0;
