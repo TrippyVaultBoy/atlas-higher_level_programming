4cd1011e1227935ec1ae319f8b72ef68ae9a5504
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 01:14:10 +0000

    changed return value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    best_key = None
     best_score = 0
     for key in a_dictionary:
         if a_dictionary[key] > best_score:
             best_score = a_dictionary[key]
-    return best_score
+            best_key = key
+    return best_key
+
