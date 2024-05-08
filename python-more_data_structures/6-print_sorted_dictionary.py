b24798f0fbb9233777fa272bcc2847980e032442
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 00:45:57 +0000

    added code to 6-print_sorted_dictionary.py

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dictionary = sorted(a_dictionary.keys())
+    for key in sorted_dictionary:
+        print("{key}: {value}".format(key, a_dictionary[key]))
