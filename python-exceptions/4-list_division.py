cea9fde38f885960dfbff48d929c11ee7f52e9fb
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 03:24:12 +0000

    removed semicolon

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = [0] * list_length
 
     for i in range(list_length):
-        result = 0;
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
