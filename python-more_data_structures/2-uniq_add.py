cd281712cf09d0cc51106877125806d132ff61f8
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 00:07:08 +0000

    changed range params

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,10 +4,10 @@ def uniq_add(my_list=[]):
     unique_sum = 0
     for i in range(len(my_list)):
         unique = True
-        for j in range(len(my_list)):
+        for j in range(i + 1, len(my_list)):
             if my_list[i] == my_list[j]:
                 unique = False
                 break
-        if unique == True:
+        if unique:
             unique_sum += my_list[i]
     return unique_sum
