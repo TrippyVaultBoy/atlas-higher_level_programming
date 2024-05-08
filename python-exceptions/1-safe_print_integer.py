cd2dd819387d225d092d8cf640314071d75af415
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 02:54:12 +0000

    added else

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
+    except:
         return False
+    else:
+        return True
