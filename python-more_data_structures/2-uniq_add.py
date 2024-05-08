b108a1919ffe1a75c21695dac6fbf1436ae80fb4
Tanner Saint <tsaint2004@gmail.com>
2024-05-08 00:05:19 +0000

    added code to 2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_sum = 0
+    for i in range(len(my_list)):
+        unique = True
+        for j in range(len(my_list)):
+            if my_list[i] == my_list[j]:
+                unique = False
+                break
+        if unique == True:
+            unique_sum += my_list[i]
+    return unique_sum
