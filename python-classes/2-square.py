993417b2b35accc31a903ccfbf425db1cc836082
Tanner Saint <tsaint2004@gmail.com>
2024-05-10 23:55:39 +0000

    added code to 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains error checking"""
+
+
+class Square:
+    """This class includes error checking"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
