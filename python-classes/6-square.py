e27329e7f62e835d955512b0e78140101afd4374
Tanner Saint <tsaint2004@gmail.com>
2024-05-11 01:20:33 +0000

    fixed init syntax

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,7 +3,7 @@
 
 class Square:
     """includes private instance attribute position"""
-    def __init__(self, size=0, position(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
