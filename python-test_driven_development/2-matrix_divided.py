71b17d19d94e2fee76d98d9aacfef6f8065cb992
Tanner Saint <tsaint2004@gmail.com>
2024-05-13 23:43:30 +0000

    added more error checking

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -35,12 +35,18 @@ def matrix_divided(matrix, div):
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
 
+    if ((not isinstance(div, int)) and (not isinstance(div, float))):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
     divided_matrix = []
 
     for row in matrix:
         divided_row = []
         for divided in row:
-            divided_row.append(divided / div)
+            divided_row.append(round(divided / div, 2))
         divided_matrix.append(divided_row)
 
     return divided_matrix
