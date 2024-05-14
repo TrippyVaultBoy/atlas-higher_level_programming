74cb2c318d01860175856254a55fc276b88338da
Tanner Saint <tsaint2004@gmail.com>
2024-05-14 00:59:48 +0000

    fixed indent

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -36,7 +36,7 @@ def matrix_divided(matrix, div):
     for row in matrix:
         for element in row:
             if ((not isinstance(element, int))
-            and (not isinstance(element, float))):
+                    and (not isinstance(element, float))):
                 raise TypeError("matrix must be a matrix"
                                 " (list of lists) of integers/floats")
 
