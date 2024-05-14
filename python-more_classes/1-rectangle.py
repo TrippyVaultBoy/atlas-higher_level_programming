1feef6243fdec6659fc678bcebf97c56e9e6af8b
Tanner Saint <tsaint2004@gmail.com>
2024-05-14 22:32:56 +0000

    removed whitespace

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     """
         Creates a rectangle
     """
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -16,7 +16,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -28,7 +28,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
