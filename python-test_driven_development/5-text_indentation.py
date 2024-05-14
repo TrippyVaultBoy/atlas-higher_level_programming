f5e4adcc90ee8cee3c885fc7e31d5f0998bc82bc
Tanner Saint <tsaint2004@gmail.com>
2024-05-14 06:11:48 +0000

    added code to 5-text_indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Includes text_indentation function"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after
+    each of these characters: ., ? and :
+
+    text must be a string, otherwise raise a
+    TypeError exception with the message
+    text must be a string
+
+    There should be no space at the beginning
+    or at the end of each printed line"""
+
+    for char in text:
+        if ((char == '.') or (char == '?') or (char == ':')):
+            print("{:c}".format(char))
+            print()
+            print()
+        else:
+            print("{:c}".format(char))
