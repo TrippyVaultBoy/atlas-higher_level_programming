69fde3781904881432be58ab5faec134ff5fb2e2
Tanner Saint <tsaint2004@gmail.com>
2024-05-14 05:39:15 +0000

    fixed isinstance

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,10 +8,10 @@ def say_my_name(first_name, last_name=""):
     raise a TypeError exception with the message first_name
     must be a string or last_name must be a string"""
 
-    if not isinstance(first_name, string):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if not isinstance(last_name, string):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {:s} {:s}".format(first_name, last_name))
