5d36ab6a646ba7f8b546849067833e02e06a6f2e
Tanner Saint <tsaint2004@gmail.com>
2024-05-14 21:23:11 +0000

    added test prefix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,23 +7,26 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     
-    def max_at_end(self):
+    def test_max_at_end(self):
         self.assertEqual(max_integer(1, 2, 3), 3)
 
-    def max_at_beginning(self):
+    def test_max_at_beginning(self):
         self.assertEqual(max_integer([3, 2, 1]), 3)
 
-    def max_in_middle(self):
+    def test_max_in_middle(self):
         self.assertEqual(max_integer([1, 3, 2]), 3)
 
-    def one_neg_number(self):
+    def test_one_neg_number(self):
         self.assertEqual(max_integer([-1, 2, 3]), 3)
 
-    def all_neg_numbers(self):
+    def test_all_neg_numbers(self):
         self.assertEqual(max_integer([-1, -2, -3]), -1)
 
-    def only_one_number(self):
+    def test_only_one_number(self):
         self.assertEqual(max_integer([3]), 3)
 
-    def empty_list(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
+
+if __name__ == '__main__':
+    unittest.main()
