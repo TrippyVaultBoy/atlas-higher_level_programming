ac4c3caa21f88263d501dfbc50eaf41ddd47d758
Tanner Saint <tsaint2004@gmail.com>
2024-05-21 23:54:22 +0000

    added code to 3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Includes is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if obj is an instance of or a class inherited from"""
+    return isinstance(obj, a_class)
