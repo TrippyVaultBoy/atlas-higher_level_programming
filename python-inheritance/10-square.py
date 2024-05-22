70a545f3f1f7396c7f1526e84d47c010652aa980
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 01:28:25 +0000

    moved init

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,9 +8,9 @@ class Square(Rectangle):
     
     def __init__(self, size):
         """Initialization method"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """returns square area"""
