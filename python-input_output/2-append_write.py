b227ef8acbd093bf19df46db6a30009cd9cb007f
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 03:01:25 +0000

    added code to 2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    """Appends a string to the end of a text file (UTF-8)
+    and returns the number of chars written"""
+
+    with open(filename, 'a', encoding='utf-8') as file:
+        chars = file.write(text)
+    return chars
