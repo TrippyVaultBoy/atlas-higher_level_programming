fb8b9708948bfe2c4f568cf1d224ab2f268e16ed
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 03:41:14 +0000

    added code to 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""saves arguments to a json file"""
+import sys
+
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    args = load("add_item.json")
+except:
+    args = []
+
+for arg in sys.argv[1:]:
+    args.append(arg)
+
+save(args, "add_item.json")
