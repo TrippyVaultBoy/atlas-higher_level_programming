42b484140002981993b50b7612eaaef812504123
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 01:20:01 +0000

    added code to 9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Includes Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """Initilization method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns area of rectangle"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """Returns string rep of rectangle"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
