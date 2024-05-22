e2451bcb4aaf005175ef3b354b0957e1a87ed87f
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 04:26:14 +0000

    added code to 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Includes class Student"""
+
+
+class Student:
+    """represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation
+        of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            student_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    student_dict[attr] = getattr(self, attr)
+            return student_dict
+        
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
