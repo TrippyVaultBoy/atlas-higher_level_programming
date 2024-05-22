21a028eddf8725380d00806e8bcb3fb0c1317417
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 04:07:47 +0000

    added code to 8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Includes class_to_json function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with
+    simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object"""
+
+    if hasattr(obj, '__dict__') == False:
+        raise ValueError("")
+
+    json_dict = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+    return json_dict
