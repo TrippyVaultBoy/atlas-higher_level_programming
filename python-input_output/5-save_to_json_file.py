70e571af7c069834efb059af19383a16d17411fb
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 03:16:26 +0000

    added type check

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file with json representation"""
+    if isinstance(my_obj, set):
+        my_obj = list(my_obj)
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
