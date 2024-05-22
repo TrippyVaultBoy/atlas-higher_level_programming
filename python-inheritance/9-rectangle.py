e2f38052b2d6d47231020cba8fdaba27047f2ab8
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 01:20:35 +0000

    removed whitespace

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
     def area(self):
         """Returns area of rectangle"""
         return self.__width * self.__height
-    
+
     def __str__(self):
         """Returns string rep of rectangle"""
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
