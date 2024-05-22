d29f159b3d03e12fcae775eb1b3d5a98e8fea342
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 03:25:34 +0000

    changed permission check

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,7 +6,7 @@ import os
 
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file with json representation"""
-    if os.access("no_perm/file_7", os.W_OK) == False:
+    if not os.access("no_perm/file_7", os.W_OK):
         raise PermissionError("Permission denied: 'no_perm/file_7'")
     if isinstance(my_obj, set):
         raise TypeError("Object of type set is not JSON serializable")
