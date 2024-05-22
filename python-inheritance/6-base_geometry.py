0fbbc2cbdf212f333946271c98d441f7df76d58f
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 00:03:58 +0000

    added code to 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Includes BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """method to calculate area"""
+        raise Exception("area() is not implemented")
