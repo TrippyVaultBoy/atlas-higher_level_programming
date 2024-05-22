4bed88add21866797569bc2be68f930dbf876f40
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 03:17:50 +0000

    raised error

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file with json representation"""
     if isinstance(my_obj, set):
-        my_obj = list(my_obj)
+        raise TypeError("Object of type set is not JSON serializable")
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
