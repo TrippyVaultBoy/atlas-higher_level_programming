35573f27df36de111e73fca07514a005aa7afc17
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 03:31:02 +0000

    fixed open function call

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Creates an object from a json file"""
-    with open(filename, 'r', endcoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
