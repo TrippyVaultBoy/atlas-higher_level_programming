f26b04d95f658ab2593ac82bf31ca289d799fa05
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 00:09:09 +0000

    fixed error messages

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """validates value"""
         if isinstance(value, int) == False:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("name must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("name must be greater than 0")
