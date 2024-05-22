740c1c4239882239cd02f7da098c88b7af6ee178
Tanner Saint <tsaint2004@gmail.com>
2024-05-22 00:09:59 +0000

    fixed error message

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,4 +14,4 @@ class BaseGeometry:
         if isinstance(value, int) == False:
             raise TypeError("name must be an integer")
         if value <= 0:
-            raise ValueError("name must be greater than 0")
+            raise ValueError("age must be greater than 0")
