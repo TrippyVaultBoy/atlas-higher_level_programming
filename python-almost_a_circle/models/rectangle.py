2f1299aca9f3eee8ad254473b7d61a6e073e05cb
Tanner Saint <tsaint2004@gmail.com>
2024-05-24 22:27:26 +0000

    removed blank line

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,6 @@ from models.base import Base
 class Rectangle(Base):
     """defines the class Rectangle"""
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
