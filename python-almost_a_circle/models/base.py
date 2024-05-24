b02545dc957a889127b0ea796975a9d612b54094
Tanner Saint <tsaint2004@gmail.com>
2024-05-24 22:11:48 +0000

    added code to base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Includes class Base"""
+
+
+class Base:
+    """base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
