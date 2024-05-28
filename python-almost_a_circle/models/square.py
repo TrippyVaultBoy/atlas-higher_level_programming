b86203116eaef67f59c911e6a11e911c2f248355
Tanner Saint <tsaint2004@gmail.com>
2024-05-28 23:05:00 +0000

    removed attribute validation from i it method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,10 +8,5 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-        Square.attribute_validate(width, "width")
-        Square.attribute_validate(height, "height")
-        Square.attribute_validate(x, "x")
-        Square.attribute_validate(y, "y")
-
         self.__width = size
         self.__height = size
