297ba968dbb9895d07894a88520310df7e298ebb
Tanner Saint <tsaint2004@gmail.com>
2024-05-28 03:22:28 +0000

    changed display method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,11 +77,8 @@ class Rectangle(Base):
             for i in range(self.__y):
                 print()
         for i in range(self.__height):
-            for j in range(self.__width):
-                if self.__y == j:
-                    print(" " * self.__x, end="")
-                print("#", end="")
-            print()
+            for j in range(self.__height):
+                print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """overwrites __str__ method"""
