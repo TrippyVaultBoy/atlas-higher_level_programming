b2119c17cec96cd9e7220afad4e376ece8292e89
Tanner Saint <tsaint2004@gmail.com>
2024-05-28 23:16:32 +0000

    added size getter and setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,3 +14,15 @@ class Square(Rectangle):
     def __str__(self):
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
             type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.attribute_validate(value, "width")
+
+        self.size = size
