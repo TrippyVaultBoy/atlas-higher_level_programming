466aa925677b47290f885a6cfa62e5bdd44e7e3c
Tanner Saint <tsaint2004@gmail.com>
2024-05-28 23:01:22 +0000

    added Square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Includes Square class that inherits from Rectangle"""
+from models.Rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines class Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id)
+        super().__init__(width)
+        super().__init__(height)
+        super().__init__(x)
+        super().__init__(y)
+
+        Square.attribute_validate(width, "width")
+        Square.attribute_validate(height, "height")
+        Square.attribute_validate(x, "x")
+        Square.attribute_validate(y, "y")
+
+        self.__width = size
+        self.__height = size
+        self.__x = x
+        self.__y = y
