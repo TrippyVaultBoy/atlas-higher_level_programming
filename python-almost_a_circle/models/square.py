ff846054ba0183ce60e31da4d312ce28a871d4ad
Tanner Saint <tsaint2004@gmail.com>
2024-05-28 23:04:15 +0000

    changed __init__ method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,11 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Defines class Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id)
-        super().__init__(width)
-        super().__init__(height)
-        super().__init__(x)
-        super().__init__(y)
+        super().__init__(size, size, x, y, id)
 
         Square.attribute_validate(width, "width")
         Square.attribute_validate(height, "height")
@@ -19,5 +15,3 @@ class Square(Rectangle):
 
         self.__width = size
         self.__height = size
-        self.__x = x
-        self.__y = y
