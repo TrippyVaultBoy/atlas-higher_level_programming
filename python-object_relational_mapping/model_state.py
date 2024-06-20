c5634acaa1b0a68214ec29a6f9844ed15edd6956
Tanner Saint <tsaint2004@gmail.com>
2024-06-20 04:16:28 +0000

    added model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Defines State class
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Defines State class
+    """
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+
+    name = Column(String(i28), nullable=False)
