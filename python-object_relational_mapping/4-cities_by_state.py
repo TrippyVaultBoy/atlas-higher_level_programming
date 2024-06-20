e1a9b6638102e9c7e18d11a90aef048f1fc7bf1d
Tanner Saint <tsaint2004@gmail.com>
2024-06-20 03:31:49 +0000

    added 4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states(mysql_username, mysql_password, database_name):
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_password,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+
+    query = """
+        SELECT cities.id, cities.name, states.name FROM cities
+        INNER JOIN states ON states.id = cities.state_id
+        ORDER BY cities.id ASC
+        """
+    cursor.execute(query, (state_name,))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    list_states(mysql_username, mysql_password, database_name)
