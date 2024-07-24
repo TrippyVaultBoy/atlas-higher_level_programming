25ffdc6dd5a39ca5b7f69bac273208e82aa2f300
Tanner Saint <tsaint2004@gmail.com>
2024-07-24 03:50:09 +0000

    added 6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let rowString = '';
+      for (let j = 0; j < this.width; j++) {
+        rowString += c;
+      }
+      console.log(rowString);
+    }
+  }
+}
