00236280ba202eb31697329fb52b45eefb00e38d
Tanner Saint <tsaint2004@gmail.com>
2024-07-24 03:22:05 +0000

    changed constructor

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -3,21 +3,19 @@
 class Rectangle {
   constructor (w, h) {
     if (w >= 0 && h >= 0) {
-      return
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
-
   print() {
     for (let i = 0; i < this.height; i++) {
-      rowString = "";
+      let rowString = "";
       for (let j = 0; j < this.width; j++) {
         rowString += "X";
       }
       console.log(rowString);
     }
   }
-}
+};
 
 module.exports = Rectangle;
