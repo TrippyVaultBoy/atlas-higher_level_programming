4893591235a4d7884cd1377a9b4321fed94dee29
Tanner Saint <tsaint2004@gmail.com>
2024-07-30 22:46:21 +0000

    added error checking

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,5 +3,12 @@ const filepath = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(filepath, 'utf-8', (err, data) => {
-    console.log(data);
+    if (err) {
+        if (err.code === 'ENOENT') {
+            console.error(`Error: File not found - ${filepath}`);
+        }
+        process.exit(1);
+    } else {
+        console.log(data);
+    }
 });
