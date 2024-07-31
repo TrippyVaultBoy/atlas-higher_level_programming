94b0340b9599d5b9cfecaea6345480b33ac9def4
Tanner Saint <tsaint2004@gmail.com>
2024-07-31 00:24:31 +0000

    fixed import statement

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request.get(url, (err, response, content) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch webpage. Status code: ${response.statusCode}`);
+    return;
+  }
+  fs.writeFile(filepath, content, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    } else {
+      console.log(filepath);
+    }
+  });
+});
